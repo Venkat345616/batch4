5d106dfbe0d113a93def2d3c6035122597c9028f
Venkat345616 <gg345616@gmail.com>
2025-04-09 15:05:14 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+let express = require('express')
+let app = express()
+let PORT = 5000;
+
+
+
+app.get('/', (req, res) => {
+    res.send('Hello world!')
+})
+
+app.listen(PORT, () => {
+    console.log(`server is running at: http://localhost:${PORT}`)
+})
